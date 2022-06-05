898baccef99f9d53560e5d86866e96c15a9f75e9
kevin-neves <kevinjrneves@gmail.com>
2022-06-05 00:10:57 -0300

    eureka functional without docker

--- api/shopCart/src/main/java/com/ecommerce/shopcart/ShopCartApplication.java
+++ api/shopCart/src/main/java/com/ecommerce/shopcart/ShopCartApplication.java
@@ -3,9 +3,10 @@ package com.ecommerce.shopcart;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableDiscoveryClient
+@EnableFeignClients
 public class ShopCartApplication {
 
     public static void main(String[] args) {
