473f2acc9a9e2767c935d93792c012226c459ac0
kevin-neves <kevinjrneves@gmail.com>
2022-08-25 12:31:56 -0300

    fixed internal calls between services

--- api/shopValidator/src/main/java/com/ecommerce/shopvalidator/kafka/ReceiveKafkaMessage.java
+++ api/shopValidator/src/main/java/com/ecommerce/shopvalidator/kafka/ReceiveKafkaMessage.java
@@ -7,6 +7,7 @@ import com.ecommerce.shopvalidator.dto.ProductResponse;
 import com.ecommerce.shopvalidator.service.CustomerService;
 import com.ecommerce.shopvalidator.service.ProductService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
@@ -16,21 +17,28 @@ public class ReceiveKafkaMessage {
 
     private final SendKafkaMessage sendKafkaMessage;
 
+    @Value(value = "${customer-api}")
+    String customerAdd;
+
+    @Value(value = "${product-api}")
+    String productAdd;
+
     @KafkaListener(topics = "SHOPCART_TOPIC", groupId = "group-1")
     public void listenTopic(ShopCart shopCart) {
 
+        System.out.println("TESTANDO ENV VAR no Kafka Receiver: " + customerAdd);
         System.out.println("Received shop list: " + shopCart.getProducts().stream().map(p -> p.getName())
                 .collect(java.util.stream.Collectors.joining(", ")));
         System.out.println("Status: " + shopCart.getStatus());
 
-        CustomerResponse customerResponse = CustomerService.getCustomer(shopCart.getCpfCustomer());
+        CustomerResponse customerResponse = CustomerService.getCustomer(shopCart.getCpfCustomer(), customerAdd);
         if (customerResponse == null && customerResponse.getCpf() != null || !customerResponse.getCpf().equals(shopCart.getCpfCustomer())) {
             throw new RuntimeException("Client not found.");
         }
 
         Float valorTotal = 0F;
         for (Product product : shopCart.getProducts()) {
-            ProductResponse productResponse = ProductService.getProduto(product.getProductIdReference());
+            ProductResponse productResponse = ProductService.getProduto(product.getProductIdReference(), productAdd);
             if (productResponse == null) {
                 throw new RuntimeException("Produto invalido.");
             }
--- api/shopValidator/src/main/java/com/ecommerce/shopvalidator/service/ProductService.java
+++ api/shopValidator/src/main/java/com/ecommerce/shopvalidator/service/ProductService.java
@@ -1,12 +1,13 @@
 package com.ecommerce.shopvalidator.service;
 
 import com.ecommerce.shopvalidator.dto.ProductResponse;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.reactive.function.client.WebClient;
 
 public class ProductService {
 
-    public static ProductResponse getProduto(Integer identificador) {
-        WebClient webClient = WebClient.create("http://localhost:8082");
+    public static ProductResponse getProduto(Integer identificador, String productAdd) {
+        WebClient webClient = WebClient.create(productAdd);
 
         return webClient
                 .get()
